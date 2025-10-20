5970997d9d1cb848652c769cf38fd2662fb43019
lucieokd <146327172+lucieokd@users.noreply.github.com>
2025-10-20 18:28:59 +0200

    otp

--- echoplay/src/App.tsx
+++ echoplay/src/App.tsx
@@ -5,7 +5,8 @@ import Library from "./pages/Library";
 import Search from "./pages/Search";
 import MainLayout from "./layouts/MainLayout";
 import Signup from "./pages/SignUp";
-import ResetPassword from "./pages/ResetPassword";
+import RequestCode from "./pages/RequestCode";
+import VerifyCode from "./pages/VerifyCode";
 
 function App() {
   return (
@@ -14,7 +15,8 @@ function App() {
         {/* Login-pagina zonder layout */}
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/requestcode" element={<RequestCode />} />
+        <Route path="/verifycode" element={<VerifyCode />} />
 
         {/* Alle andere routes gebruiken de layout mét player */}
         <Route element={<MainLayout />}>
